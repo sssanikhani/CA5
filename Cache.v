af5e2a320bad3cb9ec983e57f90b2b03982daf8b
sss <sssanikhani@gmail.com>
2020-08-05 16:59:10 +0430

    Clk added to Cache

--- Cache.v
+++ Cache.v
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
-module Cache(input rst, input [14:0] address, 
+module Cache(input clk, rst, input [14:0] address, 
                 input [31:0] MMToCache0, MMToCache1, MMToCache2, MMToCache3,
                 output [14:0] addressToMM, output reg [31:0] data, output reg Hit);
     reg [0:0] Valid [0:1023];
@@ -20,41 +20,50 @@ module Cache(input rst, input [14:0] address,
         Hit = 1'b0;
     end
 
-    always @(posedge rst) begin
-        for (i = 0; i < 1024; i = i + 1) begin
-            main[i][0] = 32'b0;
-            main[i][1] = 32'b0;
-            main[i][2] = 32'b0;
-            main[i][3] = 32'b0;
-            Tag[i] = 3'b0;
-            Valid[i] = 1'b0;
+    always @(address, posedge clk, posedge rst) begin
+        if (rst) begin
+            for (i = 0; i < 1024; i = i + 1) begin
+                main[i][0] = 32'b0;
+                main[i][1] = 32'b0;
+                main[i][2] = 32'b0;
+                main[i][3] = 32'b0;
+                Tag[i] = 3'b0;
+                Valid[i] = 1'b0;
+            end
+            data = 32'b0;
+            Hit = 1'b0;
         end
-        data = 32'b0;
-        Hit = 1'b0;
-    end
-    
-    always @(address) begin
-        Hit = 0; 
-        if(Valid[address[11:2]] == 1 && Tag[address[11:2]] == address[14:12]) begin
-            data = main[address[11:2]][address[1:0]];
-            Hit = 1;
+        else if (clk) begin
+            if(~Hit) begin
+                Tag[address[11:2]] = address[14:12];
+                Valid[address[11:2]] = 1;
+                #2
+                main [address[11:2]][0] = MMToCache0;
+                main [address[11:2]][1] = MMToCache1;
+                main [address[11:2]][2] = MMToCache2;
+                main [address[11:2]][3] = MMToCache3;
+                data = main [address[11:2]][address[1:0]];
+            end
+            if(Valid[address[11:2]] == 1 && Tag[address[11:2]] == address[14:12]) begin
+                data = main[address[11:2]][address[1:0]];
+                Hit = 1;
+            end
+            else begin
+                Hit = 0;
+            end
         end
         else begin
-            Hit = 0;
-            Tag[address[11:2]] = address[14:12];
-            Valid[address[11:2]] = 1;
-            main [address[11:2]][0] = MMToCache0;
-            main [address[11:2]][1] = MMToCache1;
-            main [address[11:2]][2] = MMToCache2;
-            main [address[11:2]][3] = MMToCache3;
-            data = main [address[11:2]][address[1:0]];
-            #2
-            Hit = 1;
-        end       
-           
- 	end
+            if(Valid[address[11:2]] == 1 && Tag[address[11:2]] == address[14:12]) begin
+                data = main[address[11:2]][address[1:0]];
+                Hit = 1;
+            end
+            else begin
+                Hit = 0;
+            end
+        end
+    end
 
-    assign addressToMM = address;
+    assign addressToMM = Hit ? addressToMM : address;
 endmodule
 
 
--- TB.v
+++ TB.v
@@ -1,19 +1,25 @@
 `timescale 1ns/1ns
 module TB();
     reg [14:0] address;
-    reg rst;
+    reg rst, clk;
     wire [31:0] MMToCache [0:3];
     wire [14:0] MMAddress;
     wire Hit;
     wire [31:0] cacheData;
 
-    Cache C(rst, address, MMToCache[0], MMToCache[1], MMToCache[2], MMToCache[3], MMAddress, cacheData, Hit);
+    Cache C(clk, rst, address, MMToCache[0], MMToCache[1], MMToCache[2], MMToCache[3], MMAddress, cacheData, Hit);
     MainMemory M(rst, MMAddress, MMToCache[0], MMToCache[1], MMToCache[2], MMToCache[3]);
     
     initial begin
-        #10 rst = 0;  #10 rst = 1;  #10 rst = 0; #10 address = 1024;
+        #10 rst = 0; #10 rst = 1; #10 rst = 0; #10 clk = 0;
+        repeat (8200) begin
+            #20 clk = 1; #20 clk = 0;
+        end
+    end
+    initial begin
+        #42 address = 1024;
         repeat (8191) begin
-            #20 address = address + 1; 
+            #40 address = address + 1; 
         end
         #50;
     end
